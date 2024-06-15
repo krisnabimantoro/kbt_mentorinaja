ba8b70f647b5ed4d4a9d8600983eb59b60429f48
krisnabimantoro <krisnabmntr@gmail.com>
2024-06-15 20:09:59 +0700

    update login

--- php/login.php
+++ php/login.php
@@ -10,6 +10,15 @@ $sql ="select * from tb_user where email='$email' and password='$password'";
 
 $result = mysqli_query($conn, $sql);
 $row = mysqli_num_rows($result);
+
+if ($row>0) {
+    header("Location: ../searching.html");
+    
+}else {
+    // Login failed, show alert
+    echo "<script>alert('Password salah'); window.location.href='../login.html';</script>";
+}
+
 echo $row;
 
 
