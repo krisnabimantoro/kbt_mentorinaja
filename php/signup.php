764b5648f62d82082e35b3f9e49b98f683575f8b
krisnabimantoro <krisnabmntr@gmail.com>
2024-06-19 19:28:48 +0700

    update signup and login

--- php/signup.php
+++ php/signup.php
@@ -8,6 +8,7 @@ $email = $_POST['email'];
 $address = $_POST['address'];
 $phone = $_POST['phone'];
 $password = $_POST['password'];
+$id_role = $_POST['role'];
 
 // Hash the password for security
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
@@ -16,12 +17,14 @@ $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 $id_role = 1;
 
 // Insert user data into tb_user table
-$stmt = $conn->prepare("INSERT INTO tb_user (name, date_of_birth, email, address, regis_date, id_role, no_handphone) VALUES (?, ?, ?, ?, NOW(), ?, ?)");
-$stmt->bind_param("ssssis", $name, $dob, $email, $address, $id_role, $phone);
+$stmt = $conn->prepare("INSERT INTO tb_user (name, date_of_birth, email, address, regis_date, id_role, no_handphone, password) VALUES (?, ?, ?, ?, NOW(), ?, ?, ?)");
+$stmt->bind_param("ssssiss", $name, $dob, $email, $address, $id_role, $phone, $password);
+
 
 if ($stmt->execute()) {
     // Registration successful, redirect to login page
-    header("Location: login.html");
+    echo "<script>alert('Berhasil Membuat Akun'); window.location.href='../login.html';</script>";
+    // header("Location: ../login.html");
     exit();
 } else {
     // Registration failed, handle accordingly
